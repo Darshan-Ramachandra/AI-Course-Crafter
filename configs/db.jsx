e7dcef30dc8fcca04b286a69367d78546737d52a
Darshan <darshanrk.ai@gmail.com>
2025-04-11 22:03:16 +0530

    fixed errors

--- configs/db.jsx
+++ configs/db.jsx
@@ -2,7 +2,8 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 // console.log(process.env.DATABASE_URL);
 
-const sql = neon(process.env.NEXT_PUBLIC_DB_CONNECTION_STRING);
+const sql = neon("postgresql://neondb_owner:npg_mYJne3L7HONa@ep-frosty-salad-a55h072m-pooler.us-east-2.aws.neon.tech/CourseList?sslmode=require");
+
 
 export const db = drizzle(sql);
 
