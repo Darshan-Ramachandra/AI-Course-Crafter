c7dfc460d273fa4bf3b48e208fdf676298eced47
Darshan-Ramachandra <darshanrk.ai@gmail.com>
2025-05-30 20:08:24 +0530

    updated email id for admin

--- configs/AdminConfig.jsx
+++ configs/AdminConfig.jsx
@@ -1,6 +1,6 @@
 // config/adminConfig.js
 export const adminConfig = {
-  emails: ["darshanrk.sse24@rvce.edu.in","shreyarasale8@gmail.com"
+  emails: ["darshanrk.sse24@rvce.edu.in","shreyarasale8@gmail.com","darshanrk.ai@gmail.com"
     
   ],
 };
